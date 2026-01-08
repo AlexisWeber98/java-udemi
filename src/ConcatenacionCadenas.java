7baf8aa0e42aa58a34f23d47b412c437d39fc90a
Ale Weber <alexis.weber@mailfence.com>
2026-01-08 08:41:46 -0300

    agregar concatenacion de cadenas

--- src/ConcatenacionCadenas.java
+++ src/ConcatenacionCadenas.java
@@ -1,2 +1,14 @@
 public class ConcatenacionCadenas {
+    public static void main(String[] args) {
+        System.out.println(" ***** Concatenación de Cadenas ***** ");
+
+        String nombre = "Ana";
+        String apellido = "García";
+
+        var nombreCompleto = nombre + " " + apellido;
+        System.out.println("Nombre Completo: " + nombreCompleto);
+
+        var saludo = "Hola, " + nombreCompleto + ". Bienvenida!";
+        System.out.println(saludo);
+    }
 }
