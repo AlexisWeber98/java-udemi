3cf8b8b138a9e97ced15e9ac0386e88c833da5b2
Ale Weber <alexis.weber@mailfence.com>
2026-01-08 08:49:49 -0300

    add constants

--- src/Constantes.java
+++ src/Constantes.java
@@ -1,2 +1,12 @@
 public class Constantes {
+    public static void main(String[] args) {
+    final int DIA_DE_LA_SEMANA = 7;
+        System.out.println("DIA_DE_LA_SEMANA = " + DIA_DE_LA_SEMANA);
+    final double PI= 3.1416;
+        System.out.println("PI = " + PI);
+    final String SALUDO = "Hola a todos";
+        System.out.println("SALUDO = " + SALUDO);
+
+        System.out.println("MATH:PI = " + Math.PI);
+    }
 }
