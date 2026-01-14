ea950a765db68349b9169d1610e8d0656255ff32
Ale Weber <alexis.weber@mailfence.com>
2026-01-14 10:09:31 -0300

    add strings work

--- src/ComparacionCadenas.java
+++ src/ComparacionCadenas.java
@@ -5,6 +5,17 @@ public class ComparacionCadenas {
         var cadena1 = "Java";
         var cadena2 = "Java";
 
-        System.out.print("cadena1 == cadena2: " + (cadena1 == cadena2)); // == COMPARA REFERENCIAS EN MEMORIA
-    }
+        var cadena3 = new String("Java");
+
+        // == compara la referencia en memoria
+
+        System.out.println("Cadena1 y cadena2 son iguales en memoria? : " + (cadena1 == cadena2)); // true
+        
+        System.out.println("Cadena1 y cadena3 son iguales en memoria? : " + (cadena1 == cadena3)); // false
+
+        // Equals compara el contenido de las cadenas
+        System.out.println("Cadena1 y cadena2 son iguales en contenido? : " + (cadena1.equals(cadena2))); // true
+        System.out.println("Cadena1 y cadena3 son iguales en contenido? : " + (cadena1.equals(cadena3))); // true
+
+   }
 }
---
+++ src/MetodosDeCadenas.java
@@ -0,0 +1,32 @@
+import java.util.Locale;
+
+public class MetodosDeCadenas {
+    public static void main(String[] args) {
+        // Metodos de cadenas
+
+        String cadena1= "Hola Madrid!";
+
+        System.out.println("largo de cadena1: " + cadena1.length());
+
+        // Reemplazar caracteres
+
+        String cadena2 = cadena1.replace("o","a");
+        System.out.println("cadena2: " + cadena2);
+
+
+        // Convertir a myusculas
+
+        String cadena3 = cadena1.toUpperCase();
+        System.out.println("cadena3: " + cadena3);
+
+        // Convertir a minusculas
+
+        System.out.println("cadena1 a minusculas: " + cadena1.toLowerCase());
+
+        // eliminar espacion innecesarios
+
+        String cadena4 = "   Hola    ";
+        System.out.println("cadena4 con espacios: " + cadena4);
+        System.out.println( "cadena4 sin espacios:ManejoSubCade " + cadena4.trim());
+    }
+}
