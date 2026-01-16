e30f54a604df69c617fca43d5c634fc1af0e7956
Ale Weber <alexis.weber@mailfence.com>
2026-01-15 21:01:34 -0300

    challenge sistema de empleados por consola

---
+++ itemsAndExcercices/src/SistemaEmpleados.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class SistemaEmpleados {
+    public static void main(String[] args) {
+
+        var console = new Scanner(System.in);
+
+        System.out.print("Introduce el nombre del empleado: ");
+
+        var name = console.nextLine();
+
+        System.out.print("Introduzca la edad del empleado: ");
+
+        var age = Integer.parseInt(console.nextLine());
+
+        System.out.print("Introduzca el salario del empleado: ");
+
+        var salary = Double.parseDouble(console.nextLine());
+
+        System.out.print("¿Es jefe el empleado? (true/false): ");
+
+        var isChief = Boolean.parseBoolean(console.nextLine());
+
+        console.close();
+
+        System.out.println("\n--- Datos del Empleado ---");
+        System.out.println("Nombre: " + name);
+        System.out.println("Edad: " + age);
+        System.out.println("Salario: $ " + salary);
+        System.out.println("Es jefe: " + isChief);
+    }
+}
