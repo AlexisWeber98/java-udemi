94042f441f0d003994591d5bf40d6f0a1c9b53df
Ale Weber <alexis.weber@mailfence.com>
2026-01-23 17:44:40 -0300

    add Operadores class with basic arithmetic operations

---
+++ itemsAndExcercices/src/Operadores.java
@@ -0,0 +1,25 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+
+        System.out.println(" ----- Operadores en Java -----");
+
+        double a = 5, b = 3, result;
+
+        result = a + b;
+        System.out.println("Suma: " + result);
+
+        result = a - b;
+        System.out.println("Resta: " + result);
+
+        result = a * b;
+        System.out.println("Multiplicación: " + result);
+
+        result = a / b;
+        System.out.println("División: " + result);
+
+        result = a % b;
+        System.out.println("Módulo: " + result);
+
+    }
+}
