0d8fa022f936ab621848f62a6316ce8209a4799d
Ale Weber <alexis.weber@mailfence.com>
2026-01-26 10:03:09 -0300

    add multiple variable assignment demonstration to OperadoresDeAsignacion class

--- itemsAndExcercices/src/OperadoresDeAsignacion.java
+++ itemsAndExcercices/src/OperadoresDeAsignacion.java
@@ -14,5 +14,11 @@ public class OperadoresDeAsignacion {
 
         myNumber *= 3; // myNumber = myNumber * 3
         System.out.println("Valor de myNumber despues de *= 3: " + myNumber);
+
+        // asignacion de variables múltiples
+
+        int a = 120, b = 15, c = 32;
+
+        System.out.printf("Valores iniciales: a = %d, b = %d, c = %d", a, b, c);
     }
 }
