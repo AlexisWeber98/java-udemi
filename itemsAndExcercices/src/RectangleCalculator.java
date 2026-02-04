18a4091a0fa732df7ddd7a647c220471350dde98
Ale Weber <alexis.weber@mailfence.com>
2026-02-03 21:45:36 -0300

    refactor: update RectangleCalculator to read dimensions from user input

--- itemsAndExcercices/src/RectangleCalculator.java
+++ itemsAndExcercices/src/RectangleCalculator.java
@@ -1,7 +1,16 @@
 public class RectangleCalculator {
     public static void main(String[] args) {
-        double height = 5.0;
-        double length = 10.0;
+        var console = new java.util.Scanner(System.in);
+        double height;
+        double length;
+
+        System.out.println("Enter the height of the rectangle: ");
+        height = Double.parseDouble(console.nextLine());
+
+        System.out.println("Enter the length of the rectangle: ");
+        length = Double.parseDouble(console.nextLine());
+
+        console.close();
 
         double area = height * length;
         double perimeter = 2 * (height + length);
