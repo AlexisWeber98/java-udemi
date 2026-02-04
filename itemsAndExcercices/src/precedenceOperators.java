da622339574d6babdacf3b033ac3c9f68c365595
Ale Weber <alexis.weber@mailfence.com>
2026-02-03 21:45:43 -0300

    add precedenceOperators class to demonstrate operator precedence in Java

---
+++ itemsAndExcercices/src/precedenceOperators.java
@@ -0,0 +1,13 @@
+public class precedenceOperators {
+    public static void main(String[] args) {
+        // precedencia de operadores
+        // 1. (), []
+        // 2. -, ++, --
+        // 3. *, /, %
+        // 4. +, -
+        // 5. <, >, <=, >=
+        // 6. ==, !=
+        // 7. && , ||
+        // 8. =, +=, -=, *=, /=, %=
+    }
+}
