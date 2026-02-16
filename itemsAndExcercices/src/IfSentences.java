4f8f6c0a4565fe00aa5909cdfb42b8de46a35677
Ale Weber <alexis.weber@mailfence.com>
2026-02-16 09:12:27 -0300

    fix: correct age logic and output messages in IfSentences class

--- itemsAndExcercices/src/IfSentences.java
+++ itemsAndExcercices/src/IfSentences.java
@@ -1,11 +1,16 @@
 public class IfSentences {
     public static void main(String[] args) {
-        int age = 18;
+        int age = 11;
 
         if (age >= 18) {
             System.out.println("You are an adult.");
+        } else if (age >= 13) {
+            System.out.println("You are minor");
+
         } else {
-            System.out.println("You are a minor.");
+            System.out.println("You are a child.");
         }
     }
+
+    // --------------------------------------------------------//
 }
