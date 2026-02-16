2cf0cbba1ebf0c0021c435643a0a73d1de63517a
Ale Weber <alexis.weber@mailfence.com>
2026-02-16 18:36:06 -0300

    add MarketWithbDiscount class to calculate discounts based on purchase amount and membership status

---
+++ itemsAndExcercices/src/MarketWithbDiscount.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class MarketWithbDiscount {
+    public static void main(String[] args) {
+        int min = 1000;
+
+        boolean isMember;
+
+        int discount;
+
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("How much is your purchase?");
+        int purchaseAmount = console.nextInt();
+
+        System.out.println("Are you a member of our market? (true/false)");
+        isMember = console.nextBoolean();
+
+        console.close();
+
+        if (purchaseAmount > min && isMember) {
+            discount = 10;
+
+        } else if (isMember) {
+            discount = 5;
+        } else {
+            discount = 0;
+        }
+
+        if (discount != 0) {
+            double discountedPrice = purchaseAmount - (purchaseAmount * discount / 100.0);
+            System.out.println("You get a discount of " + discount + "%");
+            System.out.println("Your discounted price is: " + discountedPrice);
+        } else {
+            System.out.println("You don't get a discount");
+        }
+
+    }
+}
