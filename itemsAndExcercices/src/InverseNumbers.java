ff00606d71d605ceae04f8c85d11894a4eb6985e
Ale Weber <alexis.weber@mailfence.com>
2026-03-10 18:49:05 -0300

    add: implement InverseNumbers class to demonstrate a do-while loop counting down

---
+++ itemsAndExcercices/src/InverseNumbers.java
@@ -0,0 +1,13 @@
+public class InverseNumbers {
+    public static void main(String[] args) {
+
+        int counter = 15;
+
+        do {
+            System.out.println("Counter value: " + counter);
+            counter--;
+
+        } while (counter >= 0);
+
+    }
+}
