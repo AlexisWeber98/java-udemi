8863c23aadb2d3bca728dee8497bd6c071a6ae5a
Ale Weber <alexis.weber@mailfence.com>
2026-03-10 18:45:00 -0300

    add: implement DoWhile class to demonstrate a do-while loop

---
+++ itemsAndExcercices/src/DoWhile.java
@@ -0,0 +1,13 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        System.out.println("Ciclo Do-While");
+
+        int counter = 1;
+
+        do {
+            System.out.println("Counter value: " + counter);
+            counter++;
+
+        } while (counter <= 3);
+    }
+}
