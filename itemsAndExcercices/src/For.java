9ddafb68179a363079c08f6d5743ac4e29b7f290
Ale Weber <alexis.weber@mailfence.com>
2026-03-10 21:08:36 -0300

    fix: update For class to iterate from 1 to 50 and print a message

--- itemsAndExcercices/src/For.java
+++ itemsAndExcercices/src/For.java
@@ -1,8 +1,11 @@
 public class For {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 3; i++) {
+        for (int i = 1; i <= 50; i++) {
             System.out.println("i = " + i);
         }
+
+        System.out.println(" bueno, ia ta ia");
+
     }
 }
