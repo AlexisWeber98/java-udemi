789fce9e6d40f61da048953854252a65fea87cab
Ale Weber <alexis.weber@mailfence.com>
2026-03-10 22:08:01 -0300

    add: extend Acumulative class to include do-while and while loops for summation

--- itemsAndExcercices/src/Acumulative.java
+++ itemsAndExcercices/src/Acumulative.java
@@ -9,5 +9,25 @@ public class Acumulative {
 
         }
         System.out.println("result = " + result);
+
+        System.out.println(" -----------------------  ahora con do while --------------------");
+        int result2 = 0;
+        int i = 1;
+        do {
+            System.out.println("sumamos " + result2 + " + " + i);
+            result2 += i;
+            i++;
+        } while (i < 10);
+        System.out.println("result2 = " + result2);
+
+        System.out.println("-----------------------  ahora con while --------------------");
+        int result3 = 0;
+        int j = 1;
+        while (j <= 15) {
+            System.out.println("sumamos " + result3 + " + " + j);
+            result3 += j;
+            j++;
+        }
+        System.out.println("result3 = " + result3);
     }
 }
