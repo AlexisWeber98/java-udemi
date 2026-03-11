cd9122c350c4193192a7fb18961c133503e4d7e1
Ale Weber <alexis.weber@mailfence.com>
2026-03-10 22:20:23 -0300

    add: implement IterativeMenu class for user account management menu

---
+++ itemsAndExcercices/src/IterativeMenu.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class IterativeMenu {
+    public static void main(String[] args) {
+
+        int option = 0;
+
+        Scanner console = new Scanner(System.in);
+
+        while (option != 3) {
+
+            System.out.println("Bienvenido al menu de iterativos, elige una opcion");
+            System.out.println("1. Crear cuenta");
+            System.out.println("2. Eliminar cuenta");
+            System.out.println("3. Salir");
+            System.out.println("Elige una opcion: ");
+
+            option = console.nextInt();
+
+            switch (option) {
+                case 1 -> System.out.println("Creando cuenta... \n");
+
+                case 2 -> System.out.println("Eliminando cuenta...\n");
+
+                default -> System.out.println("Opcion no valida, elige una opcion del menu\n");
+            }
+        }
+        console.close();
+    }
+}
