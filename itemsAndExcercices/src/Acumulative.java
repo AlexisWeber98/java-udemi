9a36bec9392119d005543baa16c738e04cd47c49
Ale Weber <alexis.weber@mailfence.com>
2026-03-10 21:55:55 -0300

    add: implement Acumulative class to calculate and print the sum of numbers from 0 to 10

---
+++ itemsAndExcercices/src/Acumulative.java
@@ -0,0 +1,11 @@
+public class Acumulative {
+    public static void main(String[] args) {
+        int result = 0;
+
+        for (int i = 0; i <= 10; i++) {
+            result += i;
+
+        }
+        System.out.println("result = " + result);
+    }
+}
