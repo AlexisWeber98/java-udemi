b4f8f2a7fd220f8596ff5e1a7b4964fa57801025
Ale Weber <alexis.weber@mailfence.com>
2026-03-16 14:04:54 -0300

    add: create Arrays class with basic array declaration and initialization

---
+++ itemsAndExcercices/src/Arrays.java
@@ -0,0 +1,13 @@
+public class Arrays {
+    public static void main(String[] args) {
+
+        // declarar un arreglo
+        int[] numbers;
+        // inicializar un arreglo
+        numbers = new int[5];
+
+        // declarar e inicialiar un arreglo
+        int[] numbers2 = new int[5];
+
+    }
+}
