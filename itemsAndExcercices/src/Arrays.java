cb11a4b568336f65bfafb275d900356d2bf22162
Ale Weber <alexis.weber@mailfence.com>
2026-03-16 14:13:33 -0300

    add: enhance array initialization and modification examples in Arrays class

--- itemsAndExcercices/src/Arrays.java
+++ itemsAndExcercices/src/Arrays.java
@@ -9,5 +9,25 @@ public class Arrays {
         // declarar e inicialiar un arreglo
         int[] numbers2 = new int[5];
 
+        // modificacion de un arreglo
+
+        // asignar un valor a una posicion del arreglo
+        numbers[0] = 10;
+        numbers[1] = 20;
+
+        // el resto de las posiciones del arreglo se inicializan con el valor por
+        // defecto (0 para int)
+
+        numbers2[0] = 13;
+        numbers2[1] = 21;
+        numbers2[4] = 64;
+
+        System.out.println("primer elemento del arreglo numbers2: " + numbers2[0]);
+        System.out.println("segundo elemento del arreglo numbers2: " + numbers2[1]);
+        System.out.println("ultimo elemento del arreglo numbers2: " + numbers2[4]);
+
+        for (int i = 0; i < numbers2.length; i++) {
+            System.out.println("elemento en la posicion " + i + ": " + numbers2[i]);
+        }
     }
 }
