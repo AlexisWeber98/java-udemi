5a87067e8693406bd8a99bd7b79aeb7134bbfe55
Ale Weber <alexis.weber@mailfence.com>
2026-03-16 14:31:17 -0300

    add: implement user input for dynamic array size and values in Arrays class

--- itemsAndExcercices/src/Arrays.java
+++ itemsAndExcercices/src/Arrays.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Arrays {
     public static void main(String[] args) {
 
@@ -37,5 +39,25 @@ public class Arrays {
         for (int i = 0; i < numbers3.length; i++)
             System.out.println("elemento en la posicion " + i + ": " + numbers3[i]);
 
+        // inttrodicir valores a un arreglo
+
+        Scanner console = new Scanner(System.in);
+        System.out.println("\n\ningrese el tamaño del arreglo: ");
+        int size = console.nextInt();
+
+        int[] userNumbers = new int[size];
+
+        System.out.println("ingrese valores del  arreglo ");
+        for (int i = 0; i < size; i++) {
+            System.out.println("ingrese el valor para la posicion " + i + ": ");
+            userNumbers[i] = console.nextInt();
+        }
+
+        System.out.println("\nValores del arreglo ingresados por el usuario:    \n");
+        console.close();
+
+        for (int i = 0; i < userNumbers.length; i++) {
+            System.out.println("elemento en la posicion " + i + ": " + userNumbers[i]);
+        }
     }
 }
