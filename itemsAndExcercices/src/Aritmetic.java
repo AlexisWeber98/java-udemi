f7e9730baf751dab5d393f10958ca8a45430cfa9
Ale Weber <alexis.weber@mailfence.com>
2026-03-17 20:41:57 -0300

    add: implement Aritmetic class with methods for sum and subtraction operations

---
+++ itemsAndExcercices/src/Aritmetic.java
@@ -0,0 +1,31 @@
+public class Aritmetic {
+    int operation1;
+    int operation2;
+
+    public void sum() {
+        int result = operation1 + operation2;
+        System.out.println("Sum: " + result);
+    }
+
+    public void rest() {
+        int result = operation1 - operation2;
+        System.out.println("Rest: " + result);
+    }
+
+    public static void main(String[] args) {
+        Aritmetic sum = new Aritmetic();
+        sum.operation1 = 20;
+        sum.operation2 = 10;
+        sum.sum();
+        sum.rest();
+        System.out.println("\n----------------------\n");
+        // ----------------------//
+
+        Aritmetic challenge = new Aritmetic();
+        challenge.operation1 = 12;
+        challenge.operation2 = 16;
+
+        challenge.sum();
+        challenge.rest();
+    }
+}
