f6db8741d274734fba5abf75c6c49bc907bc956c
Ale Weber <alexis.weber@mailfence.com>
2026-03-17 19:47:58 -0300

    add: implement SumFunction class to perform addition of two integers

---
+++ itemsAndExcercices/src/SumFunction.java
@@ -0,0 +1,10 @@
+public class SumFunction {
+    static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(5, 10));
+        System.out.println(sum(15, 16));
+    }
+}
