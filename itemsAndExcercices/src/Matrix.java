38465bdadacc1a2dc147be7cd3f806ecd7daafa8
Ale Weber <alexis.weber@mailfence.com>
2026-03-17 09:31:47 -0300

    refactor: define ROWS and COLS constants for matrix dimensions and clean up printing logic

--- itemsAndExcercices/src/Matrix.java
+++ itemsAndExcercices/src/Matrix.java
@@ -1,7 +1,8 @@
 public class Matrix {
     public static void main(String[] args) {
         // las matrices son arreglos de dos dimensiones (filas y columnas)
-
+        final int ROWS = 3;
+        final int COLS = 3;
         int[][] matrix = new int[3][3];
 
         // modificar alores de la matriz
@@ -20,12 +21,15 @@ public class Matrix {
         System.out.println("Valor en la posición [0][0]: " + matrix[0][0]);
         System.out.println("Valor en la posición [1][1]: " + matrix[1][1]);
         System.out.println("Valor en la posición [2][2]: " + matrix[2][2] + "\n\n");
-        // imprimir la tabla completa (la matriz)
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + "\t");
+
+        // recorrer una matriz
+
+        for (int ren = 0; ren < ROWS; ren++) {
+            for (int col = 0; col < COLS; col++) {
+                System.out.println("Valor en la posición [" + ren + "][" + col + "]: " + matrix[ren][col]);
             }
-            System.out.println();
         }
+
     }
+
 }
