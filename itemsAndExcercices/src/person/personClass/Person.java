0bdee37a51f4bbd39a4e4ed311c1d5f11aa1ae3f
Ale Weber <alexis.weber@mailfence.com>
2026-03-19 21:49:29 -0300

    add: implement Person class and main method for person management

---
+++ itemsAndExcercices/src/person/personClass/Person.java
@@ -0,0 +1,33 @@
+package person.personClass;
+
+public class Person {
+    private String name;
+    private String surName;
+
+    public Person(String name, String surName) {
+        this.name = name;
+        this.surName = surName;
+    }
+
+    public void showPerson() {
+        System.out.printf("""
+                Name: %s Surname: %s
+                """, name, surName);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+}
---
+++ itemsAndExcercices/src/person/personMain/PersonMain.java
@@ -0,0 +1,30 @@
+package person.personMain;
+
+import person.personClass.Person;
+
+public class PersonMain {
+    public static void main(String[] args) {
+
+        System.out.println("Creating person...");
+
+        Person person1 = new Person("Jacinnto", "Gomez");
+        System.out.println("Person created: " + person1.getName() + " " + person1.getSurName());
+
+        person1.setName("Pepe");
+        person1.setSurName("Perez");
+
+        System.out.print("Person edited: " + person1.getName() + " " + person1.getSurName() + "\n");
+
+        // --------------------------------------------------------------------------------------------------------//
+
+        Person person2 = new Person("Pepe", "Pepazo");
+
+        System.out.println("Creating person..." + person2.getName() + " " + person2.getSurName());
+
+        person2.setName("Juan");
+        person2.setSurName("Gomez");
+
+        System.out.print("Person edited: " + person2.getName() + " " + person2.getSurName() + "\n");
+
+    }
+}
