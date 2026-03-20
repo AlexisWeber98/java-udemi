ff676de66d735d8c8f285f427f1b700f9fc219dd
Ale Weber <alexis.weber@mailfence.com>
2026-03-20 09:30:25 -0300

    add: implement AnimalMain class to demonstrate inheritance and method usage

--- itemsAndExcercices/src/animals/Animals.java
+++ itemsAndExcercices/src/animals/Animals.java
@@ -17,4 +17,31 @@ class Dog extends Animals {
     public void bark() {
         System.out.println("Woof!");
     }
+
+}
+
+class AnimalMain {
+    public static void main(String[] args) {
+        System.out.println(" example of inheritance...");
+        System.out.println("father class: Animals");
+
+        Animals animal1 = new Animals();
+        animal1.eat();
+        animal1.sleep();
+
+        // animal1.bark(); This will cause a compile-time error because the Animals
+        // class does not have the bark method.
+
+        System.out.println("\nchild class: Dog");
+
+        Dog dog1 = new Dog();
+
+        System.out.println("methods inherited from the Animals class:");
+        dog1.eat();
+        dog1.sleep();
+
+        System.out.println("own method of the Dog class:");
+        dog1.bark();
+
+    }
 }
