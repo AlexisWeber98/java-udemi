33c4b493ee9a371cfa831b82afbb289dad792384
Ale Weber <alexis.weber@mailfence.com>
2026-03-24 10:45:05 -0300

    fix: update AritmeticTest to handle division by zero and ensure completion message is displayed

--- itemsAndExcercices/src/advanced/exceptions/AritmeticTest.java
+++ itemsAndExcercices/src/advanced/exceptions/AritmeticTest.java
@@ -3,12 +3,17 @@ package advanced.exceptions;
 public class AritmeticTest {
     public static void main(String[] args) {
         try {
-            int result = Aritmethic.division(10, 0);
+            // int result = Aritmethic.division(10, 2);
+            int result = Aritmethic.division(10, 2);
             System.out.println("Result: " + result);
 
         } catch (Exception exception) {
 
             System.out.println("Error: " + exception);
+        } finally {
+            // This block will always execute, regardless of whether an exception was thrown
+            // or caught.
+            System.out.println(" the division operation is completed");
         }
 
     }
